fe1d65ec9168db6ea16dc56cd45fa9978a88415a
ezuetell <ezuetell@rhg.com>
2026-08-28 09:50:02 -0600

    Formatting and documentation

--- scripts/02-bias_adjust_era5.py
+++ scripts/02-bias_adjust_era5.py
@@ -22,12 +22,26 @@ QDM_N_QUANTILES = 10
 UID = str(uuid.uuid4())
 START_TIME = datetime.datetime.now(datetime.UTC).isoformat()
 
-gmfd = xr.open_dataset(GMFD_URI, engine = 'zarr', chunks = {}, backend_kwargs = {"storage_options": {"token": "anon"}})
+gmfd = xr.open_dataset(
+    GMFD_URI,
+    engine="zarr",
+    chunks={},
+    backend_kwargs={"storage_options": {"token": "anon"}},
+)
 # Fill extreme values
 gmfd = gmfd.sortby("latitude").chunk({"latitude": -1, "longitude": 30, "time": -1})
-gmfd = gmfd.where(gmfd["tas"] < 1000).interpolate_na(dim="latitude", method="linear").compute()
+gmfd = (
+    gmfd.where(gmfd["tas"] < 1000)
+    .interpolate_na(dim="latitude", method="linear")
+    .compute()
+)
 
-era5 = xr.open_dataset(ERA5_URI, engine = 'zarr', chunks = {}, backend_kwargs = {"storage_options": {"token": "anon"}})
+era5 = xr.open_dataset(
+    ERA5_URI,
+    engine="zarr",
+    chunks={},
+    backend_kwargs={"storage_options": {"token": "anon"}},
+)
 
 ref = gmfd.sel(time=slice(str(HISTREF_START_YEAR), str(HISTREF_STOP_YEAR)))
 hist = era5.sel(time=slice(str(HISTREF_START_YEAR), str(HISTREF_STOP_YEAR)))
